1753a0fc63d8ce5923ed8d5607e594e0b4a36315
Shishir Thapa <shishirthapa1@gmail.com>
2021-06-25 19:07:03 +0545

    create necessary componenets

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,26 @@
+import React from "react";
+import Header from "./Header";
+import AddContact from "./AddContact";
+import ContactList from "./ContactList";
 
 function App() {
+
+  const contacts = [{
+    id: "1",
+    "name": "Shishir",
+    email: "shishirthapa1@gmail.com"
+  },
+  {
+    id: "2",
+    "name": "Basanta",
+    email: "basanta@gmail.com"
+  }]
+
   return (
-    <div>
-      Hello World.
+    <div className="ui container">
+      <Header />
+      <AddContact />
+      <ContactList contacts={contacts} />
     </div>
   );
 }
